861067c67b52507613b5070306eadfd4d9aa2b53
Shahbaz1214 <shahbazhyder1214@gmail.com>
2022-02-16 08:55:28 +0000

    third commit

---
+++ variableoperations.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Shahbaz
+ */
+public class Hello {
+        public static void main(String[] args) {
+     
+         int a = 2;
+         int b = 3;
+        
+         int sum = a + b;
+         System.out.println("Sum is:" + sum);
+        
+            int sub = a - b;
+            System.out.println("Difference is:" + sub);
+       
+               int mul = a * b;
+               System.out.println("Product is:" + mul);
+
+                  float div = a / b;
+                  System.out.println("Division is:" + div);
+        
+       }
+}
